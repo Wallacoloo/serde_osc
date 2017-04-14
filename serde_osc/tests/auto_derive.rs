de82417e89aee1030c8c9f26fdb254ff942e268b
Colin Wallace <wallacoloo@gmail.com>
2017-04-14 00:17:40 -0700

    Better formatting

--- serde_osc/tests/auto_derive.rs
+++ serde_osc/tests/auto_derive.rs
@@ -4,14 +4,13 @@ extern crate serde;
 extern crate serde_osc;
 
 use std::io::Cursor;
-use serde::{Deserialize};
+use serde::Deserialize;
 use serde::bytes::ByteBuf;
 use serde_osc::de::PktDeserializer;
 
 
 #[test]
 fn auto_de() {
-    assert!(true);
     /// Struct we'll deserialize into
     #[derive(Debug, PartialEq, Deserialize)]
     struct Deserialized {
@@ -20,14 +19,15 @@ fn auto_de() {
         arg_1: f32,
         arg_2: ByteBuf,
     }
-    // Note: 0x43dc0000 is 440.0 in f32.
-    let test_input = b"\x00\x00\x00\x2C/example/path\0\0\0,ifb\0\0\0\0\x01\x02\x03\x04\x43\xdc\0\0\0\0\0\x05\xde\xad\xbe\xef\xff\x00\x00\x00";
     let expected = Deserialized {
         address: "/example/path".to_owned(),
         arg_0: 0x01020304,
         arg_1: 440.0,
         arg_2: ByteBuf::from(vec![0xde, 0xad, 0xbe, 0xef, 0xff]),
     };
+
+    // Note: 0x43dc0000 is 440.0 in f32.
+    let test_input = b"\x00\x00\x00\x2C/example/path\0\0\0,ifb\0\0\0\0\x01\x02\x03\x04\x43\xdc\0\0\0\0\0\x05\xde\xad\xbe\xef\xff\x00\x00\x00";
 
     let rd = Cursor::new(&test_input[..]);
     let mut test_de = PktDeserializer::new(rd);
