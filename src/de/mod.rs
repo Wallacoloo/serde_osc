a483ad4354caaaea2c7b5b4979a97d3844655a03
Colin Wallace <wallacoloo@gmail.com>
2017-04-18 21:09:04 -0700

    Impl de::from_read

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -1,4 +1,4 @@
-use std::io::Cursor;
+use std::io::{Cursor, Read};
 use serde;
 use error::ResultE;
 
@@ -12,10 +12,15 @@ mod prim_deserializer;
 
 pub use self::pkt_deserializer::OwnedPktDeserializer as Deserializer;
 
+pub fn from_read<D, R>(rd: R) -> ResultE<D>
+    where R: Read, D: serde::de::Deserialize
+{
+    let mut de = Deserializer::new(rd);
+    D::deserialize(&mut de)
+}
+
 pub fn from_vec<T>(vec: &Vec<u8>) -> ResultE<T>
     where T: serde::de::Deserialize
 {
-    let rd = Cursor::new(vec);
-    let mut de = Deserializer::new(rd);
-    T::deserialize(&mut de)
+    from_read(Cursor::new(vec))
 }
