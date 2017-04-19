d79ae1273e2cd3794fb579c5fdf6cc7bd7d5b25c
Colin Wallace <wallacoloo@gmail.com>
2017-04-18 21:06:45 -0700

    Example now also implements deserialization

--- examples/to_from_vec.rs
+++ examples/to_from_vec.rs
@@ -4,13 +4,13 @@ extern crate serde;
 extern crate serde_osc;
 
 use serde::bytes::ByteBuf;
-use serde_osc::ser;
+use serde_osc::{de, ser};
 
 /// Struct we'll serialize.
 /// This represents a single OSC message with three arguments:
 ///   one of type 'i', 'f' and 'b', sent in the order they appear in the struct.
-#[derive(Debug, PartialEq, Serialize)]
-struct Serialized {
+#[derive(Debug, Deserialize, Serialize)]
+struct Message {
     address: String,
     num_channels: i32,
     rate: f32,
@@ -18,12 +18,15 @@ struct Serialized {
 }
 
 fn main() {
-    let message = Serialized {
+    let message = Message {
         address: "/audio/play".to_owned(),
         num_channels: 1,
         rate: 44100.0f32,
         content: ByteBuf::from(vec![0xde, 0xad, 0xbe, 0xef])
     };
+    println!("Serializing {:?}", message);
     let as_vec = ser::to_vec(&message).unwrap();
     println!("Serialied to: {:?}", as_vec);
+    let received: Message = de::from_vec(&as_vec).unwrap();
+    println!("Received: {:?}", received);
 }
